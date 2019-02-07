61f6935a6b0b5bf730f9f2920befde72d7a6d959
Prawee Wongsa <prawee@hotmail.com>
2019-02-08 01:52:41 +0700

    using compoments folder instead single file

---
+++ components/Menu.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+export default class Menu extends Component {
+  render() {
+    return (<View><Text>Menu</Text></View>)
+  }
+}
--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 import {AppRegistry, View, Text} from 'react-native';
 import {name as appName} from './app.json';
 import React, {Component} from 'react';
-
-class Menu extends Component {
-  render() {
-    return (<View><Text>Menu</Text></View>)
-  }
-}
+import Menu from './components/Menu';
 
 class Demo extends Component {
   render() {
