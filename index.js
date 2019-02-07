1ca416006a6983d081c10e4eb2bd1045265555e6
Prawee Wongsa <prawee@hotmail.com>
2019-02-08 01:30:46 +0700

    using Menu component on Demo component

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ import {AppRegistry, View, Text} from 'react-native';
 import {name as appName} from './app.json';
 import React, {Component} from 'react';
 
+class Menu extends Component {
+  render() {
+    return (<View><Text>Menu</Text></View>)
+  }
+}
+
 class Demo extends Component {
   render() {
-    return (<View><Text>Demo</Text></View>)
+    return (
+      <View>
+        <Menu/>
+        <Text>Demo</Text>
+      </View>
+    )
   }
 }
 
