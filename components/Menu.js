1c8df1c2361146046bd8cf49f8081f5ddbb28b71
Prawee Wongsa <prawee.w@integra8t.com>
2019-02-14 16:31:40 +0700

    how to write short with arrow function

--- components/Menu.js
+++ components/Menu.js
@@ -1,8 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View, Text } from 'react-native';
 
-export default class Menu extends Component {
-  render() {
-    return (<View><Text>Menu</Text></View>);
-  }
-}
+// export default class Menu extends Component {
+//   render() {
+//     return (<View><Text>Menu</Text></View>);
+//   }
+// }
+
+// const Menu = () => 
+//   <View>
+//     <Text style={{ fontSize: 50, color: 'blue' }}>Menu</Text>
+//   </View>;
+// export default Menu;
+
+
+export default () => 
+  <View>
+    <Text style={{ fontSize: 50, color: 'blue' }}>Menu</Text>
+  </View>;
