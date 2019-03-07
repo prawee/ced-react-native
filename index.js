c95ce7497ea7ad6ccbb174a7eee78544991daa5b
Prawee Wongsa <prawee.w@integra8t.com>
2019-03-07 13:51:46 +0700

    using App to display

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { AppRegistry } from 'react-native';
 import { name as appName } from './app.json';
-// import App from './components/App';
-import About from './components/About';
+import App from './components/App';
+// import About from './components/About';
 
-AppRegistry.registerComponent(appName, () => About);
+AppRegistry.registerComponent(appName, () => App);
